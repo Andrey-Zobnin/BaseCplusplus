7f7d8058d5abb15f02b4f03f48774a3fbe51dff9
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-02-15 11:29:51 +0300

    solution 4 with namespace std

--- TaskAndSolutions/FirstprogramAndArifmetic/4task.cpp
+++ TaskAndSolutions/FirstprogramAndArifmetic/4task.cpp
@@ -10,3 +10,15 @@
 
 */
 
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+  cout << "_      _      _" << endl;
+  cout << ">(.)__ <(.)__ =(.)__" << endl;
+  cout << " (___/  (___/  (___/ " << endl;
+  return 0;
+  
+}
