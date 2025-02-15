9ff108e3e9df605c5ea30042bd6ba0548cd66e7c
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-02-15 11:51:08 +0300

    task num 2

---
+++ TaskAndSolutions/ValuesTypesOfData/2task.cpp
@@ -0,0 +1,16 @@
+/*
+
+Не запуская код, ответьте на вопрос: что выведет на экран данная программа?
+
+*/
+#include "iostream"
+#include "string"
+
+int main() 
+{
+    string a = "2";
+    string b = "4+a";
+    string c = a + b;
+    std::cout << a + c + b << std::endl;
+    return 0;
+}
