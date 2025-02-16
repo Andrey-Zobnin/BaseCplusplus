399da8d39bf26131ed45554df9d8a2a501a03189
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-02-16 10:13:43 +0300

    add logic in pattern for task num8 -- solution

--- TaskAndSolutions/ValuesTypesOfData/8task.cpp
+++ TaskAndSolutions/ValuesTypesOfData/8task.cpp
@@ -15,13 +15,9 @@ int main()
 {
 	int a, b;
 	std::cin >> a >> b;
-
-      int c = a, d = b;
-      a = d;
-      b = c;
-      int c = a, d = b;
-      a = d;
-      b = c;
+    a = a + b;
+    b = a - b;
+    a = a - b;
 	std::cout << a << " " << b << std::endl;
 	return 0;
 }
